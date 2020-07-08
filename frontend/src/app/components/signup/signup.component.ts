d79f164f9b108f1581410868baed7f3fb34c90fd
Linuja <linuja.k@creativecliks.com>
2020-07-08 10:00:20 +0400

    Signup

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
@@ -7,7 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+    email
+    name
+    password
+    password_confirmation
+
+  public error = [];
+  constructor(private http: HttpClient) { }
+
+  handleError(error) {
+    this.error = error.error.errors;
+  }
+
+  load() {
+    console.log(this.name);
+  }
+
+  handleSignup() {
+
+    let temp = {
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      password_confirmation: this.password_confirmation
+    }
+
+
+    console.log(this.name);
+
+    this.http.post('http://localhost:8000/api/signup', temp).subscribe(
+      (response) => {
+        console.log(response)
+      },
+      (error) => {
+        this.handleError(error)
+      }
+    )
+
+  }
 
   ngOnInit(): void {
   }
